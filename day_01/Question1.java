2be8a1bcc442c52604ab91a33f24f09a016e318c
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2023-07-10 17:51:40 +0530

    first commit

---
+++ day_01/Question1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Question1 {
+    public static void main(String[] args) {
+        int x = 3, y = 8;
+        System.out.println("Addition of two number : " + (x + y));
+    }
+}
