23291853fe2ab3ac48a07c91d27808963eff37a5
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2023-07-12 00:00:42 +0530

    program is updated

--- day_01/Question1.java
+++ day_01/Question1.java
@@ -1,8 +1,22 @@
+
+// 189. Rotate Array
 import java.util.*;
+import java.util.Scanner;
 
 public class Question1 {
-    public static void main(String[] args) {
-        int x = 3, y = 8;
-        System.out.println("Addition of two number : " + (x + y));
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left++] = nums[right];
+            nums[right--] = temp;
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
     }
 }
