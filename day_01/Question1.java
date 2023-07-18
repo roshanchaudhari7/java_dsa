80bc29e4f03003fcf2647077ad2646fe78bda76d
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2023-07-18 11:40:27 +0530

    added code

--- day_01/Question1.java
+++ day_01/Question1.java
@@ -4,19 +4,5 @@ import java.util.*;
 import java.util.Scanner;
 
 public class Question1 {
-    private void reverse(int[] nums, int left, int right) {
-        while (left < right) {
-            int temp = nums[left];
-            nums[left++] = nums[right];
-            nums[right--] = temp;
-        }
-    }
-
-    public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n;
-        reverse(nums, 0, n - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
-    }
+    
 }
