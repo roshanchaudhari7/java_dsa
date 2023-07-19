76ac12ae6c638feeb19549f50d6e42a8fca04395
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2023-07-19 22:59:26 +0530

    majorityelement added

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,24 @@
+
+//  169. Majority Element
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class MajorityElement {
+    static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n/2];
+        
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter nums size: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter " + n + " elements: ");
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(majorityElement(nums));
+    }
+}
