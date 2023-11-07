908cf089d3275e2df2a12cfd8584070624d13a02
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2023-11-07 07:25:43 +0530

    Added Program

---
+++ Linked List/NthNodeFromEnd.java
@@ -0,0 +1,51 @@
+public class NthNodeFromEnd {
+    public static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+        }
+    }
+    
+    public static Node removeNthFromEnd(Node head, int n) {
+        Node slow = head;
+        Node fast = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        if (fast == null) {
+            head = head.next;
+            return head;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+
+    public static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        display(a);
+        a = removeNthFromEnd(a, 2);
+        display(a);
+    }
+}
