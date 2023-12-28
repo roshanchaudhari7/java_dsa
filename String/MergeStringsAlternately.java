f512c0a9867bd5ab178419c1a84a84e64e57b31b
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2023-12-28 11:21:01 +0530

    Merge strings alternately program is added

---
+++ String/MergeStringsAlternately.java
@@ -0,0 +1,27 @@
+//      1768.Merge Strings Alternately
+
+package String;
+
+public class MergeStringsAlternately {
+    public static String mergeAlternately(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        StringBuilder ans = new StringBuilder();
+        int i = 0;
+        while(i < n || i < m){
+            if(i < n){
+                ans.append(word1.charAt(i));
+            }
+            if(i < m){
+                ans.append(word2.charAt(i));
+            }
+            i++;
+        }
+        return ans.toString();
+    }
+    public static void main(String[] args) {
+        String word1 = "abc";
+        String word2 = "pqr";
+        System.out.println("The Merge String is : " + mergeAlternately(word1, word2));
+    }
+}
