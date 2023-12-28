febf450ff1d40e7b821d3ab3af47c32f4504dada
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2023-12-28 10:53:24 +0530

    Longest palindromic substring is added

--- String/LongestPalindromicSubstring.java
+++ String/LongestPalindromicSubstring.java
@@ -1,15 +1,18 @@
+//      5. Longest Palindromic Substring
+
 package String;
 
 public class LongestPalindromicSubstring {
-    public static String longestPalindrome(String s){
-        if(s.length() <= 1) return s;
+    public static String longestPalindrome(String s) {
+        if (s.length() <= 1)
+            return s;
         int length = 1;
         String ans = s.substring(0, 1);
 
-        for(int i=0; i<s.length(); i++){
-            for(int j=i+length; j<=s.length(); j++){
-                if(j-i > length && isPalindrome(s.substring(i, j))){
-                    length = j-i;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + length; j <= s.length(); j++) {
+                if (j - i > length && isPalindrome(s.substring(i, j))) {
+                    length = j - i;
                     ans = s.substring(i, j);
                 }
             }
@@ -17,11 +20,11 @@ public class LongestPalindromicSubstring {
         return ans;
     }
 
-    public static boolean isPalindrome(String s){
-        int i=0;
-        int j=s.length()-1;
-        while(i<j){
-            if(s.charAt(i) != s.charAt(j)){
+    public static boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
                 return false;
             }
             i++;
@@ -29,6 +32,7 @@ public class LongestPalindromicSubstring {
         }
         return true;
     }
+
     public static void main(String[] args) {
         String s = "babad";
         System.out.println("Longest Palindromic Substring is : " + longestPalindrome(s));
