eb514861e9e30cf1a728b1707b630055bc6a1f6a
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2023-12-29 22:58:33 +0530

    Middle of the Linked List is added

---
+++ Linked List/MiddleOfTheLinkedList.java
@@ -0,0 +1,47 @@
+        // 876. Middle of the Linked List
+
+import java.util.List;
+
+public class MiddleOfTheLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val){
+            this.val = val;
+        }
+    }
+    public static ListNode middleNode(ListNode head) {
+        ListNode first = head;
+        ListNode second = head;
+        while(second != null && second.next != null) {
+            first = first.next;
+            second = second.next.next;
+        }
+        return first;
+    }
+
+    public static void display(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+        ListNode f = new ListNode(6);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        display(a);
+        a = middleNode(a);
+        display(a);
+    }
+}
